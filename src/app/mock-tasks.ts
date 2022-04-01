ab384e12c1d2cafe3fc63fa960f5cbae4076a58d
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-04-01 19:28:59 -0300

    segundo commit, elimine file mosk, agregue mock

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+export const TASKS = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'Abril 5 a las 12:00',
+        reminder: true
+    },
+    {
+    id: 2,
+    text: "Realizar la compra de PC",
+    day: 'Abril 6 a las 18:00',
+    reminder: true
+    },
+    {
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: 'Abril 7 a las 09:00',
+    reminder: false
+    },
+    {
+    id: 4,
+    text: "Retirar libro de libreria",
+    day: 'Abril 10 a las 15:00',
+    reminder: true
+},
+
+
+]
