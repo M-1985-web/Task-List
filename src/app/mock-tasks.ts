e2e84eeeda298958c43f362f787f34939dbee52f
M-1985-web <martinmarcelomaldonado1985@gmail.com>
2022-04-02 13:48:41 -0300

    tercer commit, se agrego mock de tareas, junto a Task.ts

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,4 +1,9 @@
-export const TASKS = [
+import { Task } from "./Task"
+
+
+
+
+export const TASKS: Task [] = [
     {
         id: 1,
         text: "Terminar primer modulo de Angular",
@@ -6,23 +11,22 @@ export const TASKS = [
         reminder: true
     },
     {
-    id: 2,
-    text: "Realizar la compra de PC",
-    day: 'Abril 6 a las 18:00',
-    reminder: true
+        id: 2,
+        text: "Realizar la compra de PC",
+        day: 'Abril 6 a las 18:00',
+        reminder: true
     },
     {
-    id: 3,
-    text: "Investigar sobre Bootstrap",
-    day: 'Abril 7 a las 09:00',
-    reminder: false
+        id: 3,
+        text: "Investigar sobre Bootstrap",
+        day: 'Abril 7 a las 09:00',
+        reminder: false
     },
     {
-    id: 4,
-    text: "Retirar libro de libreria",
-    day: 'Abril 10 a las 15:00',
-    reminder: true
-},
-
+        id: 4,
+        text: "Retirar libro de libreria",
+        day: 'Abril 10 a las 15:00',
+        reminder: true
+    }
 
 ]
